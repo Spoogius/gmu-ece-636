d224f3c11426765e891041155f0779497aed2b04
Stewart Schuler <stewart.schuler@nrl.navy.mil>
2023-04-17 10:44:08 -0400

    Added input definition for periodogram

--- Project_1/+psd/periodogram.m
+++ Project_1/+psd/periodogram.m
@@ -1,4 +1,10 @@
 function [ff,f] = periodogram( data, fs, window, do_plot )
+    % Input:
+    %   data: Real or complex column vector of signal data
+    %   fs: Sample rate of data in Hz
+    %   window: Callable function to generate a window for data. Must take
+    %       two inputs, window(dim1, dim2)
+    %   do_plot: Boolean flag to create plot or not
 
     NFFT = size(data,1);
     f = [-fs/2:fs/NFFT:fs/2-(fs/NFFT)];
