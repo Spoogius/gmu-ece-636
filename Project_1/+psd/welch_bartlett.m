f063e9c32b4e2e8c96004a269c30174494c94a7d
Stewart Schuler <stewart.schuler@nrl.navy.mil>
2023-04-17 12:36:56 -0400

    Added welch_bartlett estimator

--- Project_1/+psd/welch_bartlett.m
+++ Project_1/+psd/welch_bartlett.m
@@ -1,15 +1,38 @@
-function [outputArg1,] = welch_bartlett( data, fs, window, NFFT, overlap, do_plot )
+function [ff, f] = welch_bartlett( data, fs, window, NFFT, overlap_stride, do_plot )
     % Input:
     %   data: Real or complex column vector of signal data
     %   fs: Sample rate of data in Hz
     %   window: Callable function to generate a window for data. Must take
-    %       two inputs, window(dim1, dim2)
+    %       a single input, window(dim1)
     %   NFFT: FFT size. In the context of welch-bartlett this is the
     %       segment size.
-    %   overlap: Number of samples per overlap. Must be <= NFFT. Overlap
-    %       ratio = overlap/NFFT
+    %   overlap_stride: Number of samples per overlap. Must be <= NFFT. Overlap
+    %       ratio = overlap_stride/NFFT
     %   do_plot: Boolean flag to create plot or not
 
-    
+    assert( overlap_stride <= NFFT );
+    numSegments = ceil((size(data,1) ./ (overlap_stride) ));
+
+    % Zero pad data for last segment
+    data_padded = complex(zeros( numSegments.*NFFT, 1 ));
+    data_padded( 1:size(data,1)) = data;
+
+
+    ff = zeros( NFFT, 1 );
+    for segIdx = 0:numSegments-1
+        [ ff_new, f ] = psd.periodogram(...
+                            data_padded((segIdx.*overlap_stride) + [1:NFFT]),...
+                            fs, window, false );
+        ff = ff + ff_new;                
+    end
+    ff = ff./numSegments;
+
+    if( do_plot )
+        figure()
+        plot( f, mag2db( abs( ff ) ) );
+        xlabel('Freq (Hz)');
+        ylabel('dB' );
+    end
+
 end
 
