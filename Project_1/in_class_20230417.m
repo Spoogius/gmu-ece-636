cd5f80e3e2ca359a0359a55769b01a4349df55d1
Spoogius <Spoogius@gmail.com>
2023-04-17 15:02:53 -0400

    Added in class work example for MUSIC estimater

---
+++ Project_1/in_class_20230417.m
@@ -0,0 +1,33 @@
+clear; clc;
+% Music estimater
+
+fs = 1000;
+
+% Segment period cant be multiple of period of complex sinusoids
+N = 52;
+
+t = ( 0:1/fs:2000-1/fs);
+
+% Signals should not have the same phase
+sig = exp( j.*2.*pi.*t.*300).';
+sig = sig + exp( j.*2.*pi.*t.*360 + (pi/5)).';
+sig = sig + gen.cwgn( 0, 0.5, size(sig,1), 1 );
+Rx = helpers.autocorrelation( sig, N);
+
+[ eVec, eVal ] = eig( Rx );
+eVals = diag( eVal );
+[ eVals, eIndex ] = sort( eVals );
+
+numSignals = 2;
+
+w = (0:2*pi/N:2*pi-(2*pi/N) );
+
+for wIdx = 1:N
+    v = exp(1i.*w(wIdx).*((0:N-1)));
+    ff( 1:N, wIdx ) = v*eVec(:,1:end-numSignals)*eVec(:,1:end-numSignals)'*v';
+end
+%ff = fftshift( fft( eVec(:, eIndex(end-(numSignals-1):end) ) ) );
+
+NFFT = N;
+f = (-fs/2:fs/NFFT:fs/2-fs/NFFT);
+plot( f, mag2db( abs( 1./mean(ff,1) )) );
