19a2152ea2916c5fb2c61a640f54e1f73ffbd6ee
Stewart <stewart.schuler@gmail.com>
2023-05-01 15:42:49 -0400

    Update blackman_tukey to use correct auto correlation (r[l])

--- Project_1/+helpers/autocorrelation.m
+++ Project_1/+helpers/autocorrelation.m
@@ -1,13 +1,5 @@
-function [ Rx ] = autocorrelation( data, segment_size )
+function [ rl ] = autocorrelation( data )
 
-numSeg = floor( size(data,1)/segment_size );
-assert( numSeg >= segment_size );
-
-Rx = zeros( segment_size, segment_size );
-
-for segIdx = 0:numSeg-1
-  x = data( (segIdx*segment_size) + [1:segment_size] );
-  Rx = Rx + ( (1/numSeg) * ( x * (x') ) );
-end
+rl = conv( data, conj( data(end:-1:1) ) );
 
 end
--- Project_1/+psd/blackman_tukey.m
+++ Project_1/+psd/blackman_tukey.m
@@ -1,18 +1,16 @@
-function [ff, f] = blackman_tukey( data, fs, window, window_size, do_plot )
+function [ff, f] = blackman_tukey( data, fs, window, do_plot )
     % Input:
     %   data: Real or complex column vector of signal data
     %   fs: Sample rate of data in Hz
     %   window: Callable function to generate a window for data. Must take
     %       a single input, window(dim1)
-    %   window_size: Number of samples to use per window when computing the
-    %   auto correlation
     %   do_plot: Boolean flag to create plot or not
 
-    Rx = helpers.autocorrelation( data, window_size );
-    Rx_windowed = Rx.*window(window_size);
+    rl = helpers.autocorrelation( data );
+    rl_windowed = rl.*window(size(rl,1));
 
-    NFFT = window_size;
-    ff = mean( abs( fftshift( fft( Rx_windowed,NFFT) ) ), 2 );
+    NFFT = length( rl_windowed );
+    ff = abs( fftshift( fft( rl_windowed,NFFT) ) );
     f = [-fs/2:fs/NFFT:fs/2-(fs/NFFT)];
 
     if( do_plot )
